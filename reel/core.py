6c96152e23eab5e9f2f678d4752d818549a76288
Kenneth Reitz <me@kennethreitz.com>
2011-05-11 22:35:45 -0400

    functions

--- reel/core.py
+++ reel/core.py
@@ -4,7 +4,7 @@ import json
 
 import requests
 
-from flask import Flask, request, abort
+from flask import Flask, request, abort, render_template, redirect
 
 
 SLASH_REPLACEMENT = '\\'
@@ -14,17 +14,51 @@ app = Flask(__name__)
 
 
 
+
+def clippy(text, bgcolor='#FFFFFF'):
+    html = """
+        <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
+                width="110"
+                height="14"
+                id="clippy" >
+        <param name="movie" value="/clippy.swf"/>
+        <param name="allowScriptAccess" value="always" />
+        <param name="quality" value="high" />
+        <param name="scale" value="noscale" />
+        <param NAME="FlashVars" value="text=#{text}">
+        <param name="bgcolor" value="#{bgcolor}">
+        <embed src="/clippy.swf"
+               width="110"
+               height="14"
+               name="clippy"
+               quality="high"
+               allowScriptAccess="always"
+               type="application/x-shockwave-flash"
+               pluginspage="http://www.macromedia.com/go/getflashplayer"
+               FlashVars="text={text}"
+               bgcolor="{bgcolor}"
+        />
+        </object>
+    """
+
+    return html.format(text=text, bgcolor=gbcolor)
+
+
+
 @app.route('/', methods=['GET'])
 def index():
     """Main index view for generating urls."""
 
-    return '\o/'
+    return render_template('index.html')
+
 
 @app.route('/new', methods=['POST'])
 def get_new_url():
 
     # in_type = request.form.get('type', None)
-    return request.form.get('url').replace('/', SLASH_REPLACEMENT)
+    in_url = request.form.get('url').replace('/', SLASH_REPLACEMENT)
+
+    return redirect('/get/' + in_url)
 
 
 
