25ddcda1199412889227034a7737f4f1748d3073
Kenneth Reitz <me@kennethreitz.com>
2011-05-11 21:48:36 -0400

    getting there

--- reel/core.py
+++ reel/core.py
@@ -1,28 +1,44 @@
 # -*- coding: utf-8 -*-
 
+import json
+
 import requests
-import urllib2
-from flask import Flask, abort
+
+from flask import Flask, request, abort
+
+
+SLASH_REPLACEMENT = '\\'
 
 
 app = Flask(__name__)
+
 
 
 @app.route('/', methods=['GET'])
 def index():
     """Main index view for generating urls."""
 
-    # werzburg routing workaround
-    # ⁄ ​
-    # \u2044
-    # \xe2\x81\x84
-    # %E2%81%84%E2%80%8B
-
     return '\o/'
 
-@app.route('/generate', methods=['POST'])
-def generate_url():
-    pass
+@app.route('/new', methods=['POST'])
+def get_new_url():
+
+    # in_type = request.form.get('type', None)
+    return request.form.get('url').replace('/', SLASH_REPLACEMENT)
+
+
+
+
+
+@app.route('/new.json', methods=['POST'])
+def get_new_url_json():
+
+    in_url = request.form.get('url').replace('/', SLASH_REPLACEMENT)
+
+    d = {'url': in_url}
+
+    return json.dumps(d)
+
 
 
 @app.route('/get/<url>', methods=['GET'])
@@ -30,13 +46,13 @@ def get_url(url):
     """Gets given URL."""
 
     # cheap werzburg routing workaround
-    url = url.replace('\\', '/')
+    url = url.replace(SLASH_REPLACEMENT, '/')
 
     try:
         r = requests.get(url)
         r.raise_for_status()
 
-    except urllib2.HTTPError, why:
+    except requests.HTTPError, why:
         abort(r.status_code)
 
     return 'GET {r.url} {r.status_code}'.format(r=r)
